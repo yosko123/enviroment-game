59db8380b66f88cb4e6d4ab553e70e6e35ad0025
yosko123 <haok.vfx@gmail.com>
2021-03-13 11:07:46 +0200

    Project(Updated)
    Added Menu + Seeds. There's a problem with the script for putting seeds though.

--- HackTuesGG/Assets/PlayerControl2d.cs
+++ HackTuesGG/Assets/PlayerControl2d.cs
@@ -7,22 +7,11 @@ public class PlayerControl2d : MonoBehaviour
 
     public float _speed = 7f;
     private Rigidbody2D _rigbody;
-    public float JumpForce = 7; 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        _rigbody = GetComponent<Rigidbody2D>();
-    }
 
     // Update is called once per frame
     private void Update()
     {
         var _move = Input.GetAxis("Horizontal");
         transform.position = transform.position + new Vector3(_move * _speed * Time.deltaTime, 0, 0);
-        if(Input.GetButtonDown("Jump"))
-        {
-            _rigbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
-        }
-
     }
 }
---
+++ HackTuesGG/Assets/PutsPlant.cs
@@ -0,0 +1,50 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Dynamic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PutsPlant : MonoBehaviour
+{
+
+    public GameObject Soil;
+
+    public float xAxis;
+    public float zAxis;
+    public float yAxis;
+
+
+    public void Start()
+    {
+        GetComponent<Transform>(); 
+        Soil = GameObject.Find("razrovena_pochva");
+        xAxis = Soil.transform.position.x;
+        yAxis = Soil.transform.position.y;
+        zAxis = Soil.transform.position.x;
+        Debug.Log(xAxis);
+        Debug.Log(yAxis);
+        Debug.Log(zAxis);
+        Soil.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+
+    public void OnMouseDown()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Soil.transform.localScale = new Vector3(xAxis, yAxis, zAxis); 
+        }
+    }
+
+
+
+
+
+
+
+
+
+}
+
